bf9cb052cb4163ce26e79d7c070ec64b90a89bd9
giddy11 <edoghotugiddy@gmail.com>
2022-10-25 10:30:07 +0100

    write the test to pass

--- TDD/DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessor.cs
+++ TDD/DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessor.cs
@@ -10,7 +10,13 @@ namespace DeskBooker.Core.Tests.Processor
 
         internal DeskBookingResult BookDesk(DeskBookingRequest request)
         {
-            throw new NotImplementedException();
+            return new DeskBookingResult
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Date = request.Date
+            };
         }
     }
 }
