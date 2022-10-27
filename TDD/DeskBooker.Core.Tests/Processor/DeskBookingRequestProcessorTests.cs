2ae632b80dabe283368c166b3faa2845611c5d0f
giddy11 <edoghotugiddy@gmail.com>
2022-10-27 12:13:38 +0100

    saved a deskBooking which failed at first

--- TDD/DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
+++ TDD/DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -1,4 +1,6 @@
-﻿using DeskBooker.Core.Processor;
+﻿using DeskBooker.Core.DataInterface;
+using DeskBooker.Core.Processor;
+using Moq;
 using System;
 using Xunit;
 
@@ -6,18 +8,13 @@ namespace DeskBooker.Core.Domain.Processor
 {
     public class DeskBookingRequestProcessorTests
     {
+        private readonly DeskBookingRequest _request;
+        private readonly Mock<IDeskBookingRepository> _deskBookingRepositoryMock;
         private readonly DeskBookingRequestProcessor _processor;
 
         public DeskBookingRequestProcessorTests()
         {
-            _processor = new DeskBookingRequestProcessor();
-        }
-
-        [Fact]
-        public void ShouldReturnDeskBookingResultWithRequestValues()
-        {
-            // Arrange
-            var request = new DeskBookingRequest
+             _request = new DeskBookingRequest
             {
                 FirstName = "Gideon",
                 LastName = "Gideon",
@@ -25,18 +22,35 @@ namespace DeskBooker.Core.Domain.Processor
                 Date = new DateTime(2022, 1, 25)
             };
 
+            _deskBookingRepositoryMock = new Mock<IDeskBookingRepository>();
+
+            _processor = new DeskBookingRequestProcessor(_deskBookingRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            // Arrange
+            //var request = new DeskBookingRequest
+            //{
+            //    FirstName = "Gideon",
+            //    LastName = "Gideon",
+            //    Email = "Gideon",
+            //    Date = new DateTime(2022, 1, 25)
+            //};
+
             // repeated the first time
             //var processor = new DeskBookingRequestProcessor();
 
             // Act
-            DeskBookingResult result = _processor.BookDesk(request);
+            DeskBookingResult result = _processor.BookDesk(_request);
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(request.FirstName, result.FirstName);
-            Assert.Equal(request.LastName, result.LastName);
-            Assert.Equal(request.Email, result.Email);
-            Assert.Equal(request.Date, result.Date);
+            Assert.Equal(_request.FirstName, result.FirstName);
+            Assert.Equal(_request.LastName, result.LastName);
+            Assert.Equal(_request.Email, result.Email);
+            Assert.Equal(_request.Date, result.Date);
         }
 
         [Fact]
@@ -49,5 +63,26 @@ namespace DeskBooker.Core.Domain.Processor
 
             Assert.Equal("request", exception.ParamName);
         }
+
+        [Fact]
+        public void ShouldSaveDeskBooking()
+        {
+            DeskBooking savedDeskBooking = null;
+            _deskBookingRepositoryMock.Setup(x => x.Save(It.IsAny<DeskBooking>())).Callback<DeskBooking>(deskBooking =>
+            {
+                savedDeskBooking = deskBooking;
+            });
+
+            _processor.BookDesk(_request);
+
+            _deskBookingRepositoryMock.Verify(x => x.Save(It.IsAny<DeskBooking>()), Times.Once);
+
+            Assert.NotNull(savedDeskBooking);
+            Assert.Equal(_request.FirstName, savedDeskBooking.FirstName);
+            Assert.Equal(_request.LastName, savedDeskBooking.LastName);
+            Assert.Equal(_request.Email, savedDeskBooking.Email);
+            Assert.Equal(_request.Date, savedDeskBooking.Date);
+        }
+
     }
 }
